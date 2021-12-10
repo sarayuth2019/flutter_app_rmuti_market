0dbded6fe9f1f28f9eb4b31159101f9e3d0378c6
chaluemwuut <chaluemwut@hotmail.com>
2021-12-10 22:05:06 +0700

    update config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
   //static const String API_URL = "https://testheroku11111.herokuapp.com";
-  static const String API_URL = "http://192.168.1.15:8080";
+  // static const String API_URL = "http://192.168.1.15:8080";
   //static const String API_URL = "http://10.0.2.2:8080";
-  //static const String API_URL = "https://iotboot.com/powergroup";
+  static const String API_URL = "https://iotboot.com/powergroup";
 }
